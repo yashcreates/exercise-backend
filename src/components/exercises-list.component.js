a5a2a1fac9b187b40ac6148aa5df421cece6b41d
yashcreates <96673842+yashcreates@users.noreply.github.com>
2024-09-21 15:17:24 +0530

    Update exercises-list.component.js

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -24,7 +24,7 @@ export default class ExercisesList extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/')
+    axios.get('13.232.242.211:5000/exercises/')
       .then(response => {
         this.setState({ exercises: response.data })
       })
@@ -34,7 +34,7 @@ export default class ExercisesList extends Component {
   }
 
   deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/'+id)
+    axios.delete('13.232.242.211:5000/exercises/'+id)
       .then(response => { console.log(response.data)});
 
     this.setState({
